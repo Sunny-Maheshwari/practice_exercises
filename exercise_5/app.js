591276e0e20bfa9a9d88f286120a930a5ccf7c34
Sunny-Maheshwari <maheshwarisunny17@gmail.com>
2024-02-11 18:35:56 +0500

    Stripping white space from user name

---
+++ exercise_5/app.js
@@ -0,0 +1,8 @@
+// Store the user_name with whitespace characters
+var user_name = "\t \n Sunny Maheshwari \n \t";
+// Print the user_name with whitespace
+console.log("Original user_name:");
+console.log(user_name);
+// Strip the whitespace and print the user_name again
+console.log("\nuser_name after stripping whitespace:");
+console.log(user_name.trim());
