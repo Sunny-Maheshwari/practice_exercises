33fb50aa4c0cbdf3a79dab6748df289fb2bbdd0e
Sunny-Maheshwari <maheshwarisunny17@gmail.com>
2024-02-11 18:05:57 +0500

    Famous Quote by famous person

---
+++ exercise_3/app.js
@@ -0,0 +1,3 @@
+var famous_person = "Jaun Elia ";
+var famous_quote = "“Existence is the decline of thought.”";
+console.log(famous_person + "once said, " + famous_quote);
---
+++ exercise_3/app.ts
@@ -0,0 +1,4 @@
+let famous_person = "Jaun Elia "
+let famous_quote = "“Existence is the decline of thought.”"
+
+console.log(  famous_person + "once said, " + famous_quote)
