0af795e4cff0e26b222fc8943fa8327ba77bc9a2
Sunny-Maheshwari <maheshwarisunny17@gmail.com>
2024-02-11 18:12:48 +0500

    Famous Quote 2: Repeat Exercise 4 done

---
+++ exercise_4/app.js
@@ -0,0 +1,3 @@
+var famous_person = "Jaun Elia ";
+var message = "".concat(famous_person, " once said, \u201CThe word kills the right of ideas.\u201D");
+console.log(message);
---
+++ exercise_4/app.ts
@@ -0,0 +1,4 @@
+let famous_person = "Jaun Elia "
+let message = `${famous_person} once said, “The word kills the right of ideas.”`
+
+console.log(message)
